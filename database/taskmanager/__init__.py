ffdfa76f2942cfd972d9425dd184f329dcb9a9c8
teddylindstrom <teddy.lindstrom.work@gmail.com>
2024-08-21 17:35:26 +0000

    I am unsure of which code I need for the PP4 so I'm just making this project to save the valuable code

---
+++ database/taskmanager/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env  # noqa
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:  # Fixed indentation
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
